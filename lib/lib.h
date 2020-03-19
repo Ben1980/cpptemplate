a5a66dc2476618fc2485f806613b2806dc0ad8ce
Benjamin Mahr <ben.mahr@gmail.com>
2020-03-19 21:53:14 +0100

    compiles and test is running

--- lib/lib.h
+++ lib/lib.h
@@ -1,14 +1,16 @@
 #ifndef LIB_H
 #define LIB_H
 
+#include <doctest/doctest.h>
+
 namespace Library {
   void PrintHelloWorld();
 };
 
-TEST_SUITE("Library Test Suite") {
+//TEST_SUITE("Library Test Suite") {
     TEST_CASE ("Library test") {
-        CHECK(false);
+      CHECK(false);
     }
-}
+//}
 
 #endif
