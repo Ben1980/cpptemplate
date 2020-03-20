56b05ddd48b48ab24bc456778cac0a2392dd16fd
Benjamin Mahr <ben.mahr@gmail.com>
2020-03-20 07:37:42 +0100

    adding first doxygen output

---
+++ docs/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cpptemplate',['cpptemplate',['../index.html',1,'']]],
+  ['contributor_20covenant_20code_20of_20conduct',['Contributor Covenant Code of Conduct',['../md_CODE_OF_CONDUCT.html',1,'']]]
+];
