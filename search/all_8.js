29a08cd04d76dfa1b321e29b3837495d234fcfb4
Ben1980 <Ben1980@users.noreply.github.com>
2020-04-06 19:14:04 +0000

    [ci skip] cleanup doc 56ba9d48d60444f721e393e592dbf30406116166

--- search/all_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
-  ['result_5ftree',['result_tree',['../namespacectest2JUnit.html#af9c3357e0b49da86171ddacd01d6127f',1,'ctest2JUnit']]]
-];
