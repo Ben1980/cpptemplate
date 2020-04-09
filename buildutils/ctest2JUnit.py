efdc473d23aff878a7c4448f063934fa3b655f39
Benjamin Mahr <Ben1980@users.noreply.github.com>
2020-04-09 13:37:20 +0200

    unused code removed

--- buildutils/ctest2JUnit.py
+++ buildutils/ctest2JUnit.py
@@ -5,10 +5,7 @@ import sys
 TAGfile = open(sys.argv[1]+"/Testing/TAG", 'r')
 dirname = TAGfile.readline().strip()
 
-xmlfile = open(sys.argv[1]+"/Testing/"+dirname+"/Test.xml", 'r')
 xslfile = open(sys.argv[2], 'r')
-
-xmlcontent = xmlfile.read()
 xslcontent = xslfile.read()
 
 xmldoc = etree.parse(sys.argv[1]+"/Testing/"+dirname+"/Test.xml")
